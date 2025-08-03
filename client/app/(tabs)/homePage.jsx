aa8904dfead6f696594b353bc7811d04f0fa07a5
Damien Lo <damienlo1234@gmail.com>
2025-08-03 10:43:33 -0400

    First Working Page

---
+++ client/app/(tabs)/homePage.jsx
@@ -0,0 +1,24 @@
+import {View, Text, StyleSheet} from 'react-native'
+
+const app = () =>{
+  return(
+    <View style={styles.container}>
+      <Text style={styles.text}>HomePage</Text>
+    </View>
+  )
+}
+
+export default app
+
+const styles = StyleSheet.create({
+  container: {
+    flex: 1,
+    flexDirection: 'column',
+  },
+  text: {
+    colour: 'white',
+    fontSize: 42,
+    fontWeight: 'bold',
+    textAlign: 'center'
+  }
+})
