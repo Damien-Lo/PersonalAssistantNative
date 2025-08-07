e7e27f8854b5b1fbc23452b45fff87ea739ae5ca
Damien Lo <damienlo1234@gmail.com>
2025-08-06 22:27:20 -0400

    Home Page Navigation to Dining Page Setuped

--- client/app/_layout.tsx
+++ client/app/_layout.tsx
@@ -29,6 +29,7 @@ export default function RootLayout() {
             <Stack>
               <Stack.Screen name="index" options={{ headerShown: false }} />
               <Stack.Screen name="SamplePage" options={{ headerShown: false }} />
+              <Stack.Screen name="DiningPage" options={{ headerShown: false }} />
             </Stack>
           </CalendarProvider>
         </DishListProvider>
