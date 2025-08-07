060a9c251f175086952c49df5fb698d75874d65a
Damien Lo <damienlo1234@gmail.com>
2025-08-07 10:20:56 -0400

    Setup Tailwind for React Native Navigation

--- client/tailwind.config.js
+++ client/tailwind.config.js
@@ -1,14 +1,10 @@
-// tailwind.config.js
 /** @type {import('tailwindcss').Config} */
 module.exports = {
-  presets: [require('nativewind/preset')],
-  content: [
-    "./App.{js,jsx,ts,tsx}",
-    "./app/**/*.{js,ts,jsx,tsx}",
-    "./components/**/*.{js,ts,jsx,tsx}",
-  ],
+  // NOTE: Update this to include the paths to all files that contain Nativewind classes.
+  content: ["./App.tsx", "./components/**/*.{js,jsx,ts,tsx}"],
+  presets: [require("nativewind/preset")],
   theme: {
     extend: {},
   },
   plugins: [],
-};
+}
